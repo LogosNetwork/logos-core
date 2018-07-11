e78d8682d305ba4d836e93cc2a2acd5674ed713d
peng-pl <peng@prometheanlabs.io>
2018-07-11 19:30:58 -0400

    Filled the placeholder functions in message_validator. Compiled, not tested.

--- rai/consensus/message_validator.cpp
+++ rai/consensus/message_validator.cpp
@@ -1,16 +1,26 @@
 #include <rai/consensus/message_validator.hpp>
+#include <string>
 
-void MessageValidator::OnPublicKey(uint8_t delegate_id, const PublicKey & key)
+void MessageValidator::OnPublicKey(uint8_t delegate_id, const PublicKey & key)  throw(bls::Exception)
 {
+	std::string keystring(reinterpret_cast<const char*>(&key), CONSENSUS_PUB_KEY_SIZE);
+	PublicKeyReal k;
+	k.deserialize(keystring);
+
     BOOST_LOG (_log) << "MessageValidator - Received public key: "
-                     << key.to_string()
+                     << k.to_string()
                      << " from delegate "
                      << delegate_id;
 
-    _keys[delegate_id] = key;
+    _keys[delegate_id] = k;
 }
 
 PublicKey MessageValidator::GetPublicKey()
 {
-    return _keypair.pub;
+	std::string keystring;
+    _keypair.pub.serialize(keystring);
+    PublicKey pk;
+    memcpy(&pk[0], keystring.data(), CONSENSUS_PUB_KEY_SIZE);
+
+    return pk;
 }
--- rai/consensus/message_validator.hpp
+++ rai/consensus/message_validator.hpp
@@ -6,13 +6,19 @@
 #include <boost/log/sources/record_ostream.hpp>
 #include <boost/log/sources/logger.hpp>
 
+#include <bls/bls.hpp>
+#include "messages/common.hpp"
+
+
 class MessageValidator
 {
-
-    using Log       = boost::log::sources::logger_mt;
-    using PublicKey = rai::public_key;
-    using Keys      = std::unordered_map<uint8_t, PublicKey>;
-    using KeyPair   = rai::keypair;
+    using Log            = boost::log::sources::logger_mt;
+    using KeyPair        = bls::KeyPair;
+    using PublicKeyReal  = bls::PublicKey;
+    using PublicKeyVec   = bls::PublicKeyVec;
+    using SignatureReal  = bls::Signature;
+    using SignatureVec   = bls::SignatureVec;
+    using Keys           = std::unordered_map<uint8_t, PublicKeyReal>;
 
 public:
 
@@ -22,23 +28,58 @@ public:
         Signature signature;
     };
 
+    PublicKeyReal PublicKeyAggregate(const ParicipationMap &pmap)
+    {
+    	PublicKeyVec keyvec;
+        for(int i = 0; i < pmap.size(); ++i)
+        {
+        	if(pmap[i])
+        	{
+        		keyvec.push_back(_keys[i]);
+        	}
+        }
+        PublicKeyReal apk;
+        apk.aggregateFrom(keyvec);
+        return apk;
+    }
+
     // Aggregate sign
     template<typename MSG>
-    void Sign(MSG & message, const std::vector<DelegateSignature> & signatures)
+    void Sign(MSG & message, const std::vector<DelegateSignature> & signatures) throw(bls::Exception)
     {
         // Set participation map:
+    	PublicKeyVec keyvec;
+    	SignatureVec sigvec;
         for(auto & sig : signatures)
         {
             message.participation_map[sig.delegate_id] = true;
+            SignatureReal sigReal;
+        	string sig_str(reinterpret_cast<const char*>(&message.signature), CONSENSUS_SIG_SIZE);
+        	sigReal.deserialize(sig_str);
+        	sigvec.push_back(sigReal);
+            keyvec.push_back(_keys[sig.delegate_id]);
         }
 
         // Now sign the message with the aggregate sig
+        SignatureReal asig;
+        asig.aggregateFrom(sigvec, keyvec);
+
+    	string asig_str;
+    	asig.serialize(asig_str);
+    	memcpy(&message.signature, asig_str.data(), CONSENSUS_SIG_SIZE);
     }
 
     // Single sign
     template<typename MSG>
     void Sign(MSG & message)
-    {}
+    {
+    	string msg(reinterpret_cast<const char*>(&message), MSG::HASHABLE_BYTES);
+    	SignatureReal sig;
+    	_keypair.prv.sign(sig, msg);
+    	string sig_str;
+    	sig.serialize(sig_str);
+    	memcpy(&message.signature, sig_str.data(), CONSENSUS_SIG_SIZE);
+    }
 
     // Aggregate validation
     template<MessageType type, MessageType type2>
@@ -48,17 +89,46 @@ public:
         // to identify those delegates that
         // signed, and access their public
         // keys using _keys.
-        return true;
+
+    	//public key agg
+        auto apk = PublicKeyAggregate(message.participation_map);
+
+        //message
+    	string msg(reinterpret_cast<const char*>(&message), PostPhaseMessage<type>::HASHABLE_BYTES);
+
+    	//sig
+    	SignatureReal sig;
+    	string sig_str(reinterpret_cast<const char*>(&message.signature), CONSENSUS_SIG_SIZE);
+    	try{
+    		sig.deserialize(sig_str);
+    	}
+    	catch (const bls::Exception &)
+    	{
+    		return false;
+    	}
+        return sig.verify(apk, msg);
     }
 
     // Single validation
+    //TODO probably need another version that verifies other node's signature
     template<MessageType type>
     bool Validate(const StandardPhaseMessage<type> & message, uint8_t delegate_id)
     {
-        return true;
+    	SignatureReal sig;
+    	string msg(reinterpret_cast<const char*>(&message), StandardPhaseMessage<type>::HASHABLE_BYTES);
+    	string sig_str(reinterpret_cast<const char*>(&message.signature), CONSENSUS_SIG_SIZE);
+
+    	try{
+    		sig.deserialize(sig_str);
+    	}
+    	catch (const bls::Exception &)
+    	{
+    		return false;
+    	}
+    	return sig.verify(_keypair.pub, msg);
     }
 
-    void OnPublicKey(uint8_t delegate_id, const PublicKey & key);
+    void OnPublicKey(uint8_t delegate_id, const PublicKey & key) throw(bls::Exception);
 
     PublicKey GetPublicKey();
 
