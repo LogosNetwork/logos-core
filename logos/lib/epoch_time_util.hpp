788aea4b46e9a46a9719422678df6f386ad470ea
Gregory Tsipenyuk <greg@prometheanlabs.io>
2018-11-16 21:47:17 -0500

    Microblock proposal time is 10min

--- logos/lib/epoch_time_util.hpp
+++ logos/lib/epoch_time_util.hpp
@@ -26,8 +26,8 @@ static const Seconds EPOCH_TRANSITION_START(20); // 20 seconds
 static const Seconds EPOCH_START(20); // 20 seconds after epoch trans start
 /// 5. Epoch transition end time: 12h + 20s
 static const Seconds EPOCH_TRANSITION_END(20); // 20 seconds
-static const Minutes MICROBLOCK_PROPOSAL_TIME(1); // 10 minutes
-static const Minutes MICROBLOCK_CUTOFF_TIME(1); // 10 minutes
+static const Minutes MICROBLOCK_PROPOSAL_TIME(10); // 10 minutes
+static const Minutes MICROBLOCK_CUTOFF_TIME(10); // 10 minutes
 static const Seconds CLOCK_DRIFT(20); // 20 seconds
 static const Seconds SECONDARY_LIST_TIMEOUT(20); // 20 seconds
 static const Minutes SECONDARY_LIST_TIMEOUT_CAP(8); // 8 minutes
