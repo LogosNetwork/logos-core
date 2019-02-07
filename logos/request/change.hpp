2a98b613b1e8260ae54e64e2cef86130c44585b3
Devon White <devon@prometheanlabs.io>
2019-02-07 08:56:46 -0500

    Advance request (de)serialization methods

--- logos/request/change.hpp
+++ logos/request/change.hpp
@@ -5,6 +5,9 @@
 struct Change : Request
 {
     Change(bool & error,
+           const logos::mdb_val & mdbval);
+
+    Change(bool & error,
            std::basic_streambuf<uint8_t> & stream);
 
     Change(bool & error,
@@ -12,6 +15,8 @@ struct Change : Request
 
     boost::property_tree::ptree SerializeJson() const override;
     uint64_t Serialize(logos::stream & stream) const override;
+    void Deserialize(bool & error, logos::stream & stream) override;
+    void DeserializeDB(bool &error, logos::stream &stream) override;
 
     void Hash(blake2b_state & hash) const override;
 
