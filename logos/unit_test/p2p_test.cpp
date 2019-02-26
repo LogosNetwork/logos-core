3a394aa077b1b8c89644c24297fbe7acd8b0e0a3
p2peer <dmitry@prometheanlabs.io>
2019-02-27 00:10:51 +0300

    p2p: unit test for ConsensusP2p cache, part 1

--- logos/unit_test/p2p_test.cpp
+++ logos/unit_test/p2p_test.cpp
@@ -117,3 +117,59 @@ TEST (P2pTest, VerifyPeersInterface)
     }
 }
 
+TEST (P2pTest, VerifyCache)
+{
+    const char *argv[] = {"unit_test", "-debug=net", 0};
+    p2p_config config;
+
+    config.argc = 2;
+    config.argv = (char **)argv;
+    config.boost_io_service = 0;
+    config.test_mode = true;
+
+    config.scheduleAfterMs = [](std::function<void()> const &handler, unsigned ms)
+    {
+        printf("scheduleAfterMs called.\n");
+    };
+
+    config.userInterfaceMessage = [](int type, const char *mess)
+    {
+        printf("%s%s: %s\n", (type & P2P_UI_INIT ? "init " : ""),
+            (type & P2P_UI_ERROR ? "error" : type & P2P_UI_WARNING ? "warning" : "message"), mess);
+    };
+
+    p2p_interface p2p;
+    ConsensusP2p<ConsensusType::BatchStateBlock> cp2p(p2p,
+        [](const PostCommittedBlock<ConsensusType::BatchStateBlock> &block, uint8_t id, ValidationStatus *status) -> bool
+        {
+            printf("Validate(%d,%d) called\n", block.sequence, id);
+            status->reason = logos::process_result::gap_previous;
+            return false;
+        },
+        [](const PostCommittedBlock<ConsensusType::BatchStateBlock> &block, uint8_t id)
+        {
+            printf("ApplyUpdates(%d,%d) called\n", block.sequence, id);
+        },
+        [](const PostCommittedBlock<ConsensusType::BatchStateBlock> &block) -> bool
+        {
+            printf("BlockExists(%d,%d) called\n", block.sequence, block.primary_delegate);
+            return false;
+        });
+
+    EXPECT_EQ(p2p.Init(config), true);
+
+    PostCommittedBlock<ConsensusType::BatchStateBlock> block1;
+    block1.sequence = 1;
+    block1.primary_delegate = 5;
+
+    std::vector<uint8_t> buf0, buf;
+    block1.Serialize(buf0, true, true);
+    buf.push_back(4), buf.push_back(0), buf.push_back(0), buf.push_back(0);
+    buf.push_back(2), buf.push_back(logos_version),
+    buf.push_back((int)ConsensusType::BatchStateBlock), buf.push_back(block1.primary_delegate);
+    while (buf0.size() & 3) buf0.push_back(0);
+    buf.push_back(buf0.size() & 0xff), buf.push_back(buf0.size() / 0x100), buf.push_back(0), buf.push_back(0);
+    buf.insert(buf.end(), buf0.begin(), buf0.end());
+
+    EXPECT_EQ(cp2p.ProcessInputMessage(buf.data(), buf.size()), true);
+}
