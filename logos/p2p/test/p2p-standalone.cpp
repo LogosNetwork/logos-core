d6f46d0dd3be26b1b1101f7c477a66a7fa6a6772
p2peer <dmitry@prometheanlabs.io>
2019-05-15 23:21:23 +0300

    p2p: restore help output in p2p-standalone; auto create .logos dir

--- logos/p2p/test/p2p-standalone.cpp
+++ logos/p2p/test/p2p-standalone.cpp
@@ -9,6 +9,7 @@
 #include <ctype.h>
 #include <pthread.h>
 #include <signal.h>
+#include <sys/stat.h>
 #include <boost/move/utility_core.hpp>
 #include <boost/log/expressions.hpp>
 #include <boost/log/sources/logger.hpp>
@@ -21,6 +22,10 @@
 #include <boost/asio.hpp>
 #include "../../../lmdb/libraries/liblmdb/lmdb.h"
 #include "../p2p.h"
+
+#define OPTHEAD     26
+#define OPTWIDTH    80
+#define OPTMAX      4096
 
 class p2p_standalone : public p2p_interface
 {
@@ -84,8 +89,50 @@ int main(int argc, char **argv)
     pthread_t thread;
     void *res;
 
-    printf("This is p2p standalone application. Initializing...\n");
+    printf("This is p2p standalone application.\n");
+    if (argc == 1 || (argc == 2
+                      && (!strcmp(argv[1], "-h") || !strcmp(argv[1], "-?") || !strcmp(argv[1], "-help") || !strcmp(argv[1], "--help"))))
+    {
+        printf("Usage: %s options...\nOptions:\n", argv[0]);
+        p2p.TraverseCommandLineOptions([](const char *option, const char *description, int flags)
+        {
+            char buf[OPTMAX];
+            int pos, len, i, c;
+            sprintf(buf, "  -%s%-*s", option, OPTHEAD - 3 - strlen(option), flags & P2P_OPTION_ARGUMENT ? "=arg" : "    ");
+            pos = len = strlen(buf);
+            while (*description)
+            {
+                if (pos >= OPTWIDTH)
+                {
+                    i = 0;
+                    if (*description != ' ')
+                        while (buf[len - i - 1] != ' ')
+                            ++i;
+                    if (i)
+                        memmove(buf + len + OPTHEAD + 1 - i, buf + len - i, i);
+                    c = buf[len + OPTHEAD + 1 - i];
+                    len += sprintf(buf + len - i, "\n%*s", OPTHEAD, ""), pos = OPTHEAD;
+                    if (i)
+                        buf[len - i] = c;
+
+                    if (*description == ' ')
+                    {
+                        ++description;
+                        continue;
+                    }
+                }
+                buf[len++] = *description++;
+                pos++;
+            }
+            buf[len++] = 0;
+            printf("%s\n", buf);
+        });
+        return 1;
+    }
+
+    printf("Initializing...\n");
     signal(SIGTTIN, SIG_IGN);
+    mkdir(".logos", 0770);
 
     config.argc = argc;
     config.argv = argv;
@@ -106,11 +153,11 @@ int main(int argc, char **argv)
     config.boost_io_service = &io_service;
     config.scheduleAfterMs = std::bind(&::scheduleAfterMs, std::placeholders::_1, std::placeholders::_2);
     config.userInterfaceMessage = [](int type, const char *mess)
-        {
-            printf("\n%s%s: %s\nType message: ", (type & P2P_UI_INIT ? "init " : ""),
-                   (type & P2P_UI_ERROR ? "error" : type & P2P_UI_WARNING ? "warning" : "message"), mess);
-            fflush(stdout);
-        };
+    {
+        printf("\n%s%s: %s\nType message: ", (type & P2P_UI_INIT ? "init " : ""),
+               (type & P2P_UI_ERROR ? "error" : type & P2P_UI_WARNING ? "warning" : "message"), mess);
+        fflush(stdout);
+    };
 
     err = mdb_env_create(&config.lmdb_env);
     if (err)
