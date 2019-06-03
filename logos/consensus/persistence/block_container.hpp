6c8f099cf9a4971c47ecca427209dfe6f930a0ef
p2peer <dmitry@prometheanlabs.io>
2019-06-03 23:30:02 +0300

    [LOGOS-121] Approproate members moved to block_container.

--- logos/consensus/persistence/block_container.hpp
+++ logos/consensus/persistence/block_container.hpp
@@ -28,27 +28,58 @@ private:
     ChainPtr    next;
 };
 
-class EpochPeriod
+class BlockCache;
+
+class PendingBlockContainer
 {
+public:
     using RBPtr = std::shared_ptr<ApprovedRB>;
     using MBPtr = std::shared_ptr<ApprovedMB>;
     using EBPtr = std::shared_ptr<ApprovedEB>;
 
-private:
-    uint32_t                        epoch_num;
-    EBPtr                           eb;
-    std::list<MBPtr>                mbs;
-    std::unordered_set<BlockHash>   rbs_next_mb_depend_on;
-    std::list<RBPtr>                rbs[NUM_DELEGATES];
-};
+    struct EpochPeriod
+    {
+        EpochPeriod(uint32_t epoch_num)
+            : epoch_num(epoch_num)
+            , eb(nullptr)
+        {
+        }
+        EpochPeriod(EBPtr block)
+            : epoch_num(block->epoch_number)
+            , eb(block)
+        {
+        }
+        EpochPeriod(MBPtr block)
+            : epoch_num(block->epoch_number)
+            , eb(nullptr)
+        {
+            mbs.push_front(block);
+        }
+        EpochPeriod(RBPtr block)
+            : epoch_num(block->epoch_number)
+            , eb(nullptr)
+        {
+            assert(block->primary_delegate < NUM_DELEGATES);
+            rbs[block->primary_delegate].push_front(block);
+        }
 
-class PendingBlockContainer
-{
+        uint32_t                        epoch_num;
+        EBPtr                           eb;
+        std::list<MBPtr>                mbs;
+        std::unordered_set<BlockHash>   rbs_next_mb_depend_on;
+        std::list<RBPtr>                rbs[NUM_DELEGATES];
+
+        //TODO optimize
+        //1 for each unprocessed tip of the oldest mb
+        //std::bitset<NUM_DELEGATES> mb_dependences;
+    };
 private:
     std::list<EpochPeriod>                          epochs;
     std::unordered_set<BlockHash>                   cached_blocks;
     std::unordered_map<BlockHash, ChainPtr>         hash_dependency_table;
     std::unordered_map<AccountAddress, ChainPtr>    account_dependency_table;
+
+    friend class BlockCache;
 };
 
 }
