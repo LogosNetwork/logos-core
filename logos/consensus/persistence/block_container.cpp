459713033362173532357912f17eec3b73841493
p2peer <dmitry@prometheanlabs.io>
2019-06-08 23:52:58 +0300

    [LOGOS-121] Adding hash depencences if validation is failed

--- logos/consensus/persistence/block_container.cpp
+++ logos/consensus/persistence/block_container.cpp
@@ -1,1 +1,20 @@
 #include "block_container.hpp"
+
+namespace logos {
+
+void PendingBlockContainer::AddDependency(const BlockHash &hash, EBPtr block)
+{
+    hash_dependency_table.insert(std::make_pair(hash, ChainPtr(block)));
+}
+
+void PendingBlockContainer::AddDependency(const BlockHash &hash, MBPtr block)
+{
+    hash_dependency_table.insert(std::make_pair(hash, ChainPtr(block)));
+}
+
+void PendingBlockContainer::AddDependency(const BlockHash &hash, RBPtr block)
+{
+    hash_dependency_table.insert(std::make_pair(hash, ChainPtr(block)));
+}
+
+}
