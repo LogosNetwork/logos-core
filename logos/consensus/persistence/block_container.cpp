9c26b5ea5ad82a2a54d07524188c389851d0311c
p2peer <dmitry@prometheanlabs.io>
2019-06-23 23:02:45 +0300

    [LOGOS-121] Refactoring Validate() function, first version

--- logos/consensus/persistence/block_container.cpp
+++ logos/consensus/persistence/block_container.cpp
@@ -277,7 +277,165 @@ bool PendingBlockContainer::MarkAsValidated(RBPtr block)
     return res;
 }
 
-bool PendingBlockContainer::DumpCachedBlocks()
+bool PendingBlockContainer::GetNextBlock(ChainPtr &ptr, uint8_t &rb_idx, bool success)
+{
+    LOG_TRACE(log) << "PendingBlockContainer::"<<__func__<<"{";
+
+    uint32_t epoch_number;
+
+    if (ptr.rptr)
+    {
+        epoch_number = ptr.rptr->block->epoch_number;
+        rb_idx = ptr.rptr->block->primary_delegate;
+    }
+    else if (ptr.mptr)
+    {
+        epoch_number = ptr.mptr->block->epoch_number;
+    }
+    else if (ptr.eptr)
+    {
+        epoch_number = ptr.eptr->block->epoch_number;
+    }
+    else
+    {
+        epoch_number = -1u;
+    }
+
+    assert(rb_idx<=NUM_DELEGATES);
+
+    std::lock_guard<std::mutex> lck (chains_mutex);
+
+    auto e = epochs.begin();
+    while (e != epochs.end())
+    {
+        if (epoch_number != -1u && e->epoch_num != epoch_number)
+            continue;
+
+        epoch_number = -1u;
+
+        if (!ptr.mptr && !ptr.eptr)
+        {
+            if (ptr.rptr)
+            {
+                if (success)
+                    e->rbs[rb_idx].pop_front();
+                else
+                    e->rbs[rb_idx].front()->lock = false;
+                ptr.rptr = nullptr;
+            }
+
+            uint num_rb_chain_no_progress = 0;
+            // try rb chains till num_rb_chain_no_progress reaches NUM_DELEGATES
+            while (num_rb_chain_no_progress < NUM_DELEGATES)
+            {
+                for (;;)
+                {
+                    std::list<RPtr>::iterator to_validate = e->rbs[rb_idx].begin();
+                    if (to_validate == e->rbs[rb_idx].end()
+                            || !(*to_validate)->continue_validate
+                            || (*to_validate)->lock)
+                    {
+                        //cannot make progress with empty list
+                        num_rb_chain_no_progress++;
+                        rb_idx = (rb_idx + 1) % NUM_DELEGATES;
+                        break;//for(;;)
+                    }
+                    else
+                    {
+                        ptr.rptr = *to_validate;
+                        ptr.rptr->continue_validate = false;
+                        ptr.rptr->lock = true;
+                        return true;
+                    }
+                }
+            }
+        }
+
+        bool mbs_empty = e->mbs.empty();
+        bool last_mb = false;
+        if (!ptr.eptr)
+        {
+            if (ptr.mptr)
+            {
+                if (success) {
+                    e->mbs.pop_front();
+                    last_mb = ptr.mptr->block->last_micro_block;
+                    if (last_mb)
+                        assert(e->mbs.empty());
+                }
+                else
+                    e->mbs.front()->lock = false;
+                ptr.mptr = nullptr;
+            }
+
+            if (!e->mbs.empty()
+                    && e->mbs.front()->continue_validate
+                    && !e->mbs.front()->lock)
+            {
+                ptr.mptr = e->mbs.front();
+                ptr.mptr->continue_validate = false;
+                ptr.mptr->lock = true;
+                return true;
+            }
+        }
+
+        bool e_finished = false;
+
+        if (ptr.eptr)
+        {
+            if (success)
+            {
+                e->eb = nullptr;
+                auto e_old = e;
+                ++e;
+                epochs.erase(e_old);
+                e_finished = true;
+            }
+            else
+                e->eb->lock = false;
+            ptr.eptr = nullptr;
+        }
+
+        if ((last_mb || mbs_empty) &&
+            e->eb && e->eb->continue_validate && !e->eb->lock)
+        {
+            ptr.eptr = e->eb;
+            ptr.eptr->continue_validate = false;
+            ptr.eptr->lock = true;
+            return true;
+        }
+
+        if (e_finished)
+        {
+            continue;
+        }
+        else
+        {
+            break;
+#if 0
+            //two-tip case, i.e. first 10 minutes of the latest epoch
+            bool last_epoch_begin = block_container.epochs.size()==2 &&
+                    e->eb == nullptr &&
+                    e->mbs.empty() &&
+                    (++e)->mbs.empty();
+            if(!last_epoch_begin)
+            {
+                e = block_container.epochs.end();
+            }
+            else
+            {
+                //already did ++e
+            }
+#endif
+        }
+    }
+
+    LOG_ERROR(log) << "BlockCache::"<<__func__<<"}";
+
+    return false;
+}
+
+void PendingBlockContainer::DumpCachedBlocks()
 {
     std::lock_guard<std::mutex> lck (cache_blocks_mutex);
     LOG_TRACE(log) << "BlockCache::"<<__func__<<": cached hashes: " << cached_blocks.size();
