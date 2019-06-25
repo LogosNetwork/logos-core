ee16a8a44eb55b3d35602d5efebbae1aa8dc9436
Stephen <stephen@prometheanlabs.io>
2019-06-25 15:44:53 -0400

    [LOGOS-126] Recommit overwritten genesis request signature fix from 4/26/19

--- logos/identity_management/delegate_identity_manager.cpp
+++ logos/identity_management/delegate_identity_manager.cpp
@@ -320,8 +320,8 @@ DelegateIdentityManager::CreateGenesisAccounts(logos::transaction &transaction)
                      pair.pub,                    // link/to
                      amount,
                      0,                           // transaction fee
-                     pair.prv.data,
-                     pair.pub, // SG: Sign with correct key
+                     logos::test_genesis_key.prv.data, // SG: Sign with correct key
+                     logos::test_genesis_key.pub,
                      work);
 
         genesis_account.SetBalance(genesis_account.GetBalance() - amount,0,transaction);
