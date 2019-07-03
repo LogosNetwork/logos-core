38cd0b4da0991f30bc56edf1cf875a7ecbeef3bc
p2peer <dmitry@prometheanlabs.io>
2019-07-03 23:08:11 +0300

    [LOGOS-121] VerifyTest, part 2

--- logos/unit_test/block_cache.cpp
+++ logos/unit_test/block_cache.cpp
@@ -21,17 +21,25 @@ static RBPtr make_rb(int epoch_num, uint8_t delegate_id) {
     return rb;
 }
 
-static MBPtr make_mb(int epoch_num, uint8_t delegate_id) {
+static MBPtr make_mb(int epoch_num, uint8_t delegate_id, int sequence, const BlockHash &previous) {
     MBPtr mb = std::make_shared<ApprovedMB>();
     mb->epoch_number = epoch_num;
     mb->primary_delegate = delegate_id;
+    mb->sequence = sequence;
+    mb->previous = previous;
     return mb;
 }
 
-static EBPtr make_eb(int epoch_num, uint8_t delegate_id) {
+static EBPtr make_eb(int epoch_num, uint8_t delegate_id, const Tip &micro_tip) {
     EBPtr eb = std::make_shared<ApprovedEB>();
     eb->epoch_number = epoch_num;
     eb->primary_delegate = delegate_id;
+    eb->micro_block_tip = micro_tip;
+    extern Delegate init_delegate(AccountAddress account, Amount vote, Amount stake, bool starting_term);
+    for (int i = 0; i < NUM_DELEGATES; i++)
+    {
+        eb->delegates[i] = init_delegate(0, 0, 0, 0);
+    }
     return eb;
 }
 
@@ -43,6 +51,33 @@ TEST (BlockCache, VerifyTest)
     boost::filesystem::path const data_path(TEST_DB);
     logos::block_store store(error, data_path);
     EXPECT_EQ(error, false);
+
+    EBPtr e0 = make_eb(2, 0, Tip());
+    MBPtr m0 = make_mb(3, 1, 0, BlockHash());
+
+    Tip etip;
+    etip.epoch = 2;
+    etip.sqn = 0;
+    etip.digest = e0->Hash();
+
+    Tip mtip;
+    mtip.epoch = 3;
+    mtip.sqn = 0;
+    mtip.digest = m0->Hash();
+
+    {
+        logos::transaction t(store.environment, nullptr, true);
+
+        Tip etip;
+        etip.epoch = 2;
+        etip.sqn = 0;
+        etip.digest = e0->Hash();
+
+        store.epoch_put(*e0, t);
+        store.epoch_tip_put(etip, t);
+        store.micro_block_put(*m0, t);
+        store.micro_block_tip_put(mtip, t);
+    }
 
     logos::BlockWriteQueue q(store, true);
     {
@@ -57,8 +92,8 @@ TEST (BlockCache, VerifyTest)
     }
     {
         ValidationStatus s;
-        s.progress = 2;
-        MBPtr mb = make_mb(4, 8);
+        s.progress = 0;
+        MBPtr mb = make_mb(3, 8, 1, m0->Hash());
         EXPECT_EQ(q.VerifyAggSignature(mb), true);
         EXPECT_EQ(q.VerifyContent(mb, &s), true);
         std::cout << "MB status: " << ProcessResultToString(s.reason) << std::endl;
@@ -67,13 +102,13 @@ TEST (BlockCache, VerifyTest)
     }
     {
         ValidationStatus s;
-        s.progress = 3;
-        EBPtr eb = make_eb(5, 9);
+        s.progress = 0;
+        EBPtr eb = make_eb(3, 9, mtip);
         EXPECT_EQ(q.VerifyAggSignature(eb), true);
         EXPECT_EQ(q.VerifyContent(eb, &s), true);
         std::cout << "EB status: " << ProcessResultToString(s.reason) << std::endl;
-        //EXPECT_EQ(q.BlockExists(eb), false);
-        //EXPECT_EQ(q.IsBlockCached(eb->Hash()), false);
+        EXPECT_EQ(q.BlockExists(eb), false);
+        EXPECT_EQ(q.IsBlockCached(eb->Hash()), false);
     }
     std::cout << "VerifyTest end" << std::endl;
 }
